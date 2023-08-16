16038b2495daea676485fbb9a1b977da01965695
io <gie9ohbeixah@paperboats.net>
2023-08-16 00:17:27 +0000

    bump patch version to 0.0.4

--- pleroma.py
+++ pleroma.py
@@ -12,7 +12,7 @@ from pathlib import Path, PurePath
 from datetime import datetime, timezone
 from dateutil.parser import parse as parsedate
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 class BadRequest(Exception):
 	pass
